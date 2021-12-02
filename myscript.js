563e5467fca20ad86548256519770c3497e14ac9
Milan Stojko <milanmilance2000.ms@gmail.com>
2021-12-02 15:37:37 +0100

    concluso

---
+++ myscript.js
@@ -0,0 +1,7 @@
+
+
+let NomeValue= prompt("inserici il tuo nome");
+let CognomeValue= prompt("inserisci il tuo cognome");
+let ColoreValue= prompt("inserisci il tuo colore preferito");
+const ValoreValue= 21;
+ document.getElementById("nuova_password").innerHTML = NomeValue + CognomeValue + ColoreValue + ValoreValue
